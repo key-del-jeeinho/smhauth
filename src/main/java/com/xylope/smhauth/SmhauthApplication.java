bbf7f42d7d8b83b2ce905057dabd70991548d870
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-08-28 16:55:35 +0900

    Initial commit

---
+++ src/main/java/com/xylope/smhauth/SmhauthApplication.java
@@ -0,0 +1,13 @@
+package com.xylope.smhauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmhauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmhauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xylope/smhauth/SmhauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xylope.smhauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmhauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
